784a011483dd7046c5e8bfc7d563119cce8b6221
vullnetsherifi <vs31791@seeu.edu.mk>
2026-01-30 18:41:01 +0100

    added migration

--- src/Library.Api/Program.cs
+++ src/Library.Api/Program.cs
@@ -113,5 +113,12 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+app.Run();
 
 app.Run();
