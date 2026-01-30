6e07d60b9a11df6a4360fb897a90dda3d2d4eac4
vullnetsherifi <vs31791@seeu.edu.mk>
2026-01-30 20:20:35 +0100

    addeda debug controller

---
+++ src/Library.Api/Controllers/DebugController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/debug")]
+public class DebugController : ControllerBase
+{
+    private readonly IConfiguration _config;
+    public DebugController(IConfiguration config) => _config = config;
+
+    [HttpGet("conn")]
+    public IActionResult Conn()
+    {
+        var cs = _config.GetConnectionString("DefaultConnection");
+        return Ok(new
+        {
+            hasConnectionString = !string.IsNullOrWhiteSpace(cs),
+            startsWithHost = cs?.Contains("Host=") == true
+        });
+    }
+}
